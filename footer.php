baa22206c08f0346c0077bc5d460f0cd2ec7717a
Arbaaz Ali <arbaaz@webmingo.in>
2019-10-05 17:22:57 +0530

    startup

---
+++ footer.php
@@ -0,0 +1,6 @@
+
+  <script src="js/library/jquery-v3/jquery.js"></script>
+  <script src="js/library/jquery-v3/poppers.js"></script>
+  <script src="js/library/bootstrap-v4/bootstrap.js"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Welcome</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/library/bootstrap-v4/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css">
+</head>
+<body>
+	<header>
+		<div class="top-bar">
+			<div class="container">
+				<div class="row">
+					<div class="col-xl-8 col-lg-8 col-sm-4 col-7"></div>
+					<div class="col-xl-4 col-lg-4 col-sm-4 col-5">
+						<div class="top-bar-right"> 
+							<ul> 
+								<li> 
+									<a href="#" class="text-dark">
+										<i class="fas fa-user"></i>
+										<span>Register</span>
+									</a> 
+								</li> 
+								<li> 
+									<a href="#" class="text-dark">
+										<i class="fas fa-sign-in-alt"></i> 
+										<span>Login</span>
+									</a> 
+								</li> 
+							</ul> 
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
