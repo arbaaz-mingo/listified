6d70ae17f5532f705e13963e5c3f77509e3962ab
Arbaaz Ali <arbaaz@webmingo.in>
2019-10-23 13:01:11 +0530

    Header

--- header.php
+++ header.php
@@ -15,60 +15,107 @@
 				<div class="row">
 					<div class="col-xl-8 col-lg-8 col-sm-4 col-7">
 						<div class="top-bar-left">
-							<ul class="socials"> 
-								<li> 
+							<ul class="socials">
+								<li>
 									<a class="social-icon" href="#">
 										<i class="fab fa-facebook-f"></i>
-									</a> 
-								</li> 
-								<li> 
+									</a>
+								
+								</li>
+								<li>
 									<a class="social-icon" href="#">
 										<i class="fab fa-twitter"></i>
-									</a> 
-								</li> 
-								<li> 
+									</a>
+								
+								</li>
+								<li>
 									<a class="social-icon" href="#">
 										<i class="fab fa-linkedin-in"></i>
-									</a> 
-								</li> 
-								<li> 
+									</a>
+								
+								</li>
+								<li>
 									<a class="social-icon" href="#">
 										<i class="fab fa-instagram"></i>
-									</a> 
-								</li> 
+									</a>
+								
+								</li>
 							</ul>
 						</div>
 					</div>
 					<div class="col-xl-4 col-lg-4 col-sm-4 col-5">
-						<div class="top-bar-right"> 
-							<ul> 
-								<li> 
-									<a href="#" class="text-dark">
+						<div class="top-bar-right">
+							<ul>
+								<li>
+									<a href="#">
 										<i class="fas fa-user"></i>
 										<span>Register</span>
-									</a> 
-								</li> 
-								<li> 
-									<a href="#" class="text-dark">
+									</a>
+								
+								</li>
+								<li>
+									<a href="#">
 										<i class="fas fa-sign-in-alt"></i> 
 										<span>Login</span>
-									</a> 
-								</li> 
+									</a>
+								
+								</li>
 								<li>
 									<div class="dropdown">
-  										<button type="button" class="btn-dash dropdown-toggle" data-toggle="dropdown">
+										<button type="button" class="btn-dash dropdown-toggle" data-toggle="dropdown">
     										<i class="fas fa-home"></i> Dashboard
   										</button>
-  										<div class="dropdown-menu">
-    										<a class="dropdown-item" href="#"><i class="fas fa-user"></i> My Profile</a>
-    										<a class="dropdown-item" href="#"><i class="fas fa-user"></i> My Ads</a>
-    										<a class="dropdown-item" href="#"><i class="fas fa-bell"></i> Notifications</a>
-    										<a class="dropdown-item" href="#"><i class="fas fa-power-off"></i> Logout</a>
-  										</div>
+									
+										<div class="dropdown-menu">
+											<a class="dropdown-item" href="#"><i class="fas fa-user"></i> My Profile</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-user"></i> My Ads</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-bell"></i> Notifications</a>
+											<a class="dropdown-item" href="#"><i class="fas fa-power-off"></i> Logout</a>
+										</div>
 									</div>
 								</li>
-							</ul> 
+							</ul>
 						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="middle-bar">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-12">
+						<nav class="navbar navbar-expand-lg">
+							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+						
+							<a class="navbar-brand" href="#"><img src="images/logo.png" class="img-fluid"></a>
+
+							<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+								<ul class="navbar-nav m-auto mt-2 mt-lg-0">
+									<li class="nav-item active">
+										<a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">About Us</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Categories</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Blog</a>
+									</li>
+									<li class="nav-item">
+										<a class="nav-link" href="#">Contact Us</a>
+									</li>
+								</ul>
+								<form class="form-inline my-2 my-lg-0">
+									<ul class="side-btn">
+										<li><a href="#"><i class="fas fa-pen-alt"></i> Post An Ad</a></li>
+									</ul>
+								</form>
+							</div>
+						</nav>
 					</div>
 				</div>
 			</div>
