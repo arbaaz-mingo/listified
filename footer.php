f815c5a54036a4e71cd4dbe52d656a75db494520
Arbaaz Ali <arbaaz@webmingo.in>
2019-10-23 18:55:42 +0530

    App footer

--- footer.php
+++ footer.php
@@ -1,4 +1,23 @@
-
+<footer>
+	<div class="subscriber-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-xs-12 col-sm-12 col-md-12 col-lg-push-1 col-lg-10">
+					<div class="newsletter">
+						<h2>Signup For Newsletter:</h2>
+						<form class="formnewsletter">
+							<fieldset>
+								<i class="far fa-envelope-open"></i>
+								<input type="email" name="email" class="form-control" placeholder="Enter your email here">
+								<button type="button">Signup Now</button>
+							</fieldset>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
   <script src="js/library/jquery-v3/jquery.js"></script>
   <script src="js/library/jquery-v3/poppers.js"></script>
   <script src="js/library/bootstrap-v4/bootstrap.js"></script>
